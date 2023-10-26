653f6b1b2e3e8c36ebdf683a5f9b30d92822ff8e
vyk <none@example.com>
2023-10-26 13:52:41 +1100

    Add more jest mocks

--- src/tests/mocks.ts
+++ src/tests/mocks.ts
@@ -1,3 +1,6 @@
+jest.mock("k6", () => ({}), { virtual: true });
+jest.mock("k6/http", () => fakeHttp, { virtual: true });
+jest.mock("src/common/core/http", () => ({ http: fakeHttp }));
 jest.mock(
   "k6",
   () => ({
@@ -9,3 +12,29 @@ jest.mock(
   }),
   { virtual: true }
 );
+jest.mock(
+  "k6/metrics",
+  () => ({
+    Rate: jest.fn().mockImplementation(() =>
+      // Works and lets you check for constructor calls
+      ({ constructor: () => {} })
+    ),
+    Counter: jest.fn().mockImplementation(() => ({ constructor: () => {} })),
+  }),
+  { virtual: true }
+);
+jest.mock("k6/crypto", () => {}, { virtual: true });
+
+export const fakeHttp: {
+  postCalls: { data: any; url: string; headers: any }[];
+  post(url: string, data: string, headers: any): void;
+} = {
+  postCalls: [],
+  post(url: string, data: string, headers: any) {
+    this.postCalls.push({
+      url,
+      data,
+      headers,
+    });
+  },
+};
